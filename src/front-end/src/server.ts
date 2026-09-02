208e0fa16051a230b4656ffceca1cfbc2a62a8ec
luiz-rissardi <rissardi.luiz2006@gmail.com>
2026-09-02 16:32:34 -0300

    chore(infra) completed an configuration of angular to front-end

--- src/front-end/src/server.ts
+++ src/front-end/src/server.ts
@@ -10,7 +10,11 @@ import { join } from 'node:path';
 const browserDistFolder = join(import.meta.dirname, '../browser');
 
 const app = express();
-const angularApp = new AngularNodeAppEngine();
+const angularApp = new AngularNodeAppEngine(
+  {
+    allowedHosts: ['localhost', 'localhost:4000', 'your-production-domain.com']
+  }
+);
 
 /**
  * Example Express Rest API endpoints can be defined here.
